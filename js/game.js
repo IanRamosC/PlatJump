88cad092eafbc4e176f533fd0000470fe6e29d65
Ian Ramos <ianramosc@gmail.com>
2015-07-14 21:58:24 -0300

    Added mobile support(partial)

--- js/game.js
+++ js/game.js
@@ -1,3 +1,6 @@
+var isMobile = false;
+if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+    || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
 var score = 0
   , stars
   , scoreText
@@ -33,8 +36,10 @@ Jumper.Play.prototype = {
 
     // scaling
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-    this.scale.maxWidth = this.game.width;
-    this.scale.maxHeight = this.game.height;
+    if(isMobile === false) {
+      this.scale.maxWidth = this.game.width;
+      this.scale.maxHeight = this.game.height;
+    }
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
     this.scale.setScreenSize( true );
@@ -169,7 +174,7 @@ Jumper.Play.prototype = {
     // basic hero setup
     this.hero = game.add.sprite( this.world.centerX, this.world.height - 36, 'hero' );
     this.hero.anchor.set( 0.5 );
-    
+
     // track where the hero started and how much the distance has changed from that point
     this.hero.yOrig = this.hero.y;
     this.hero.yChange = 0;
@@ -185,29 +190,29 @@ Jumper.Play.prototype = {
 
   heroMove: function() {
     // handle the left and right movement of the hero
-    if( this.cursor.left.isDown ) {
+    if( this.cursor.left.isDown || (this.input.pointer1.x < 150 && this.input.pointer1.isDown)) {
       this.hero.body.velocity.x = -200;
-    } else if( this.cursor.right.isDown ) {
+    } else if( this.cursor.right.isDown || (this.input.pointer1.x > 150 && this.input.pointer1.isDown)) {
       this.hero.body.velocity.x = 200;
     } else {
       this.hero.body.velocity.x = 0;
     }
 
     // handle hero jumping
-    if (this.cursor.up.isDown) {
+    if (this.cursor.up.isDown || this.input.pointer1.isDown) {
       this.hero.movingUp = true;
     }
     if( this.hero.movingUp === true && this.hero.body.touching.down ) {
       soundFx.jump.play();
       this.hero.body.velocity.y = -350;
-    } 
-    
+    }
+
     // wrap world coordinated so that you can warp from left to right and right to left
     this.world.wrap( this.hero, this.hero.width / 8, false );
 
     // track the maximum amount that the hero has travelled
     this.hero.yChange = Math.max( this.hero.yChange, Math.abs( this.hero.y - this.hero.yOrig ) );
-    
+
     // if the hero falls below the camera view, gameover
     if( this.hero.y > this.cameraYMin + this.game.height && this.hero.alive ) {
       this.state.start( 'Play' );
@@ -219,7 +224,7 @@ Jumper.Play.prototype = {
 //   create: function() {
 //     this.stage.backgroundColor = '#000';
 //     this.add.text(150, 250, 'Loading...', { font: '14px Arial', fill: '#FFF' });
-    
+
 //   }
 // }
 
